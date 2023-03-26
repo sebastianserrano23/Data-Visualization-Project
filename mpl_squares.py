74018f34ee749f29b97774d406a44375c881cb02
Sebastian Serrano <serranosebastian23@gmail.com>
2023-03-26 10:18:45 -0700

    imported matplotlib.pyplot to serve as my library for this project, right now since I am just learning, I created a simple line chart with 4 points and opened it with the plt.show() function

---
+++ mpl_squares.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+
+fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
+
+# set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+plt.show()
